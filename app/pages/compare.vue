0f29177fd23d3fa9e37e7af9b1a1d3c7b2f8da99
Antoine Gourgue <antoine.gourgue@epitech.eu>
2025-11-06 14:36:06 +0100

    fix: add watch import in compare page

--- app/pages/compare.vue
+++ app/pages/compare.vue
@@ -94,7 +94,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue'
+import { ref, computed, watch } from 'vue'
 import { useTicker } from '~/composables/useTicker'
 import { useKlines } from '~/composables/useKlines'
 import { useSymbolsUniverse } from '~/composables/useSymbolsUniverse'
